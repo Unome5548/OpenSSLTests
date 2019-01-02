6fb6019dd95d8712005e3b78a6cfd3e48e39e35b
Michael Patterson <michaelpatterson@michaels-mbp.ltsp>
2019-01-02 14:24:19 -0700

    Initial Commit

---
+++ C_Code_TestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  C_Code_TestApp
+//
+//  Created by Michael Patterson on 1/2/19.
+//  Copyright © 2019 Microsoft. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
